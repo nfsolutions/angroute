961dacc9dab2e1e8c9ccd43f4ed60b371eea76ef
nydi <stefan.nydegger@gmail.com>
2018-07-08 14:00:08 +0200

    settings without loading

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -16,7 +16,7 @@ export class ContactService {
       setTimeout(() => {
         observer.next(contacts);
         observer.complete();
-      }, 5000);
+      }, 3000);
     });
   }
 
@@ -25,7 +25,7 @@ export class ContactService {
       setTimeout(() => {
         observer.next(contacts.find((contact) => contact.id == id));
         observer.complete();
-      }, 5000);
+      }, 3000);
     });
   }
 }
