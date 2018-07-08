1edce90dab8740e281d8893b08554bba0aeb2446
nydi <stefan.nydegger@gmail.com>
2018-07-08 21:08:03 +0200

    mandator, not used yet

---
+++ src/app/mandator/mandator.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-mandator',
+  templateUrl: './mandator.component.html',
+  styleUrls: ['./mandator.component.css']
+})
+export class MandatorComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  save() {
+
+  }
+
+}
